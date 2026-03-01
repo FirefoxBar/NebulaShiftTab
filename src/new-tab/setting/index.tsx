31fe7563e0a4fe2df49f6a6f7832bcb78ecf9ba4
ShuangYa <i@sylingd.com>
2026-03-01 12:19:49 +0800

    feat: enhance setting load

--- src/new-tab/setting/index.tsx
+++ src/new-tab/setting/index.tsx
@@ -1,20 +1,31 @@
 import type React from 'react';
-import { lazy, Suspense, useState } from 'react';
+import { lazy, Suspense, useEffect, useState } from 'react';
 import { SettingIcon } from './icon';
 
 const SettingSideSheet = lazy(() => import('./content'));
 
 const Setting: React.FC = () => {
   const [visible, setVisible] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
-  const toggleVisible = () => setVisible(!visible);
+  useEffect(() => {
+    if (visible && !loaded) {
+      setLoaded(true);
+    }
+  }, [visible]);
+
+  const loadSideSheet = visible || loaded;
 
   return (
     <>
-      <div className="setting-icon" onClick={toggleVisible}>
+      <div
+        className="setting-icon"
+        onClick={() => setVisible(true)}
+        onMouseEnter={() => setLoaded(true)}
+      >
         <SettingIcon />
       </div>
-      {visible && (
+      {loadSideSheet && (
         <Suspense fallback={null}>
           <SettingSideSheet visible={visible} setVisible={setVisible} />
         </Suspense>

32c9c9a1a6962719c8a98545ab63a50af83e6e1f
ShuangYa <i@sylingd.com>
2026-03-19 19:40:55 +0800

    chore: remove contextMenu id

--- src/background/context-menus.ts
+++ src/background/context-menus.ts
@@ -5,8 +5,6 @@ import { prefs } from '@/share/prefs';
 import { SearchItemAlias } from '@/share/type-alias';
 import { getNewTabs } from './utils';
 
-const prefix = 'nebula-shift-tab-';
-
 const noop = () => {
   if (chrome.runtime.lastError) {
     // ignore
@@ -60,7 +58,7 @@ export function initContextMenus() {
       }
       chrome.contextMenus.create(
         {
-          id: `${prefix}search-${search[SearchItemAlias.key]}`,
+          id: `search-${search[SearchItemAlias.key]}`,
           contexts: ['selection'],
           title: search[SearchItemAlias.name],
         },
@@ -70,7 +68,7 @@ export function initContextMenus() {
 
     chrome.contextMenus.create(
       {
-        id: `${prefix}add-to-home`,
+        id: 'add-to-home',
         contexts: ['link'],
         title: t('addToHome'),
       },
@@ -79,7 +77,7 @@ export function initContextMenus() {
 
     chrome.contextMenus.create(
       {
-        id: `${prefix}add-page-to-home`,
+        id: 'add-page-to-home',
         contexts: ['page'],
         title: t('addCurrentPageToHome'),
       },
@@ -90,20 +88,16 @@ export function initContextMenus() {
   chrome.contextMenus.onClicked.addListener((info, tab) => {
     const { selectionText, linkUrl, pageUrl } = info;
     const menuItemId = String(info.menuItemId);
-    if (!menuItemId.startsWith(prefix)) {
-      return;
-    }
-    const id = menuItemId.substring(prefix.length);
-    if (id === 'add-page-to-home' && pageUrl && tab) {
+    if (menuItemId === 'add-page-to-home' && pageUrl && tab) {
       handleAddSite(tab.title || '', pageUrl);
       return;
     }
-    if (id === 'add-to-home' && linkUrl) {
+    if (menuItemId === 'add-to-home' && linkUrl) {
       handleAddSite(selectionText || '', linkUrl);
       return;
     }
-    if (id.startsWith('search-') && selectionText) {
-      const searchKey = id.substring(7);
+    if (menuItemId.startsWith('search-') && selectionText) {
+      const searchKey = menuItemId.substring(7);
       handleSearch(searchKey, selectionText);
       return;
     }

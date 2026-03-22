7c0209963b870e5e29407950d6364e3cf9bc1f23
ShuangYa <i@sylingd.com>
2026-03-22 12:33:46 +0800

    chore: remove code

--- src/new-tab/index.tsx
+++ src/new-tab/index.tsx
@@ -3,8 +3,6 @@ import './init';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { StorageKey } from '@/share/constant';
-import { getAndWatch, local } from '@/share/storage';
 import { Search } from './search';
 import Setting from './setting';
 import { Sites } from './sites';
@@ -20,15 +18,6 @@ const Page = () => (
     <Setting />
   </>
 );
-
-const bgImgEl = document.createElement('div');
-bgImgEl.className = 'main-bg-img';
-document.body.appendChild(bgImgEl);
-getAndWatch(local, StorageKey.bg, x => {
-  if (x) {
-    bgImgEl.style.backgroundImage = `url(${x})`;
-  }
-});
 
 const rootEl = document.getElementById('root');
 if (rootEl) {

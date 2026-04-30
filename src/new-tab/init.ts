e9eb87d16b2588dad7d7301f085b43d54b9227aa
ShuangYa <i@sylingd.com>
2026-04-30 23:21:39 +0800

    refactor: theme handler

--- src/new-tab/init.ts
+++ src/new-tab/init.ts
@@ -3,6 +3,92 @@ import isDarkMode from '@/share/is-dark-mode';
 import { prefs } from '@/share/prefs';
 import { getAndWatch, local } from '@/share/storage';
 
+function handleStyleVar() {
+  const styleEl = document.createElement('style');
+  document.head.appendChild(styleEl);
+  let updatingStyle = false;
+  function updateStyle() {
+    if (updatingStyle) {
+      return;
+    }
+    updatingStyle = true;
+    requestAnimationFrame(() => {
+      const width = prefs.get('siteWidth');
+      const gap = prefs.get('siteGap');
+      const size = prefs.get('siteSize');
+      styleEl.innerHTML = `body{--site-area-width:${width}px;--site-area-gap:${gap}px;--site-icon-size:${size}px}`;
+      updatingStyle = false;
+    });
+  }
+
+  prefs.watchKey('siteGap', updateStyle);
+  prefs.watchKey('siteSize', updateStyle);
+  prefs.watchKey('siteWidth', updateStyle);
+  updateStyle();
+}
+
+function handleDarkMode() {
+  const bgEl = document.createElement('div');
+  bgEl.className = 'main-bg';
+  document.body.appendChild(bgEl);
+
+  function updateDark(dark: number) {
+    if (dark === 0) {
+      bgEl.style.backgroundColor = 'transparent';
+      return;
+    }
+    if (dark < 0) {
+      const abs = Math.abs(dark);
+      if (abs < 10) {
+        bgEl.style.backgroundColor = `rgba(255,255,255,0.0${abs})`;
+        return;
+      }
+      bgEl.style.backgroundColor = `rgba(255,255,255,0.${abs})`;
+      return;
+    }
+    if (dark < 10) {
+      bgEl.style.backgroundColor = `rgba(0,0,0,0.0${dark})`;
+      return;
+    }
+    bgEl.style.backgroundColor = `rgba(0,0,0,0.${dark})`;
+  }
+
+  prefs.getAndWatch('background', ({ dark, dark2, blur }) => {
+    updateDark(isDarkMode() ? dark2 : dark);
+    bgEl.style.backdropFilter = blur ? `blur(${blur / 5}px)` : '';
+  });
+
+  prefs.getAndWatch('darkMode', () => {
+    const nowDark = document.body.hasAttribute('theme-mode');
+    const is = isDarkMode();
+    if (is && !nowDark) {
+      document.body.setAttribute('theme-mode', 'dark');
+      updateDark(prefs.get('background').dark2);
+    }
+    if (!is && nowDark) {
+      document.body.removeAttribute('theme-mode');
+      updateDark(prefs.get('background').dark);
+    }
+  });
+}
+
+function handleBackgroundImage() {
+  const bgImgEl = document.createElement('div');
+  bgImgEl.className = 'main-bg-img';
+  document.body.appendChild(bgImgEl);
+  getAndWatch(local, StorageKey.bg, x => {
+    if (x) {
+      bgImgEl.style.backgroundImage = `url(${x})`;
+    }
+  });
+}
+
+function handleOther() {
+  prefs.getAndWatch('theme', theme =>
+    document.body.setAttribute('data-theme', theme),
+  );
+}
+
 function initThemeHandler() {
   const bgEl = document.createElement('div');
   bgEl.className = 'main-bg';
@@ -50,37 +136,9 @@ function initThemeHandler() {
       updateDark(prefs.get('background').dark);
     }
   });
-
-  const styleEl = document.createElement('style');
-  document.head.appendChild(styleEl);
-  let updatingStyle = false;
-  function updateStyle() {
-    if (updatingStyle) {
-      return;
-    }
-    updatingStyle = true;
-    requestAnimationFrame(() => {
-      const width = prefs.get('siteWidth');
-      const gap = prefs.get('siteGap');
-      const size = prefs.get('siteSize');
-      styleEl.innerHTML = `body{--site-area-width:${width}px;--site-area-gap:${gap}px;--site-icon-size:${size}px}`;
-      updatingStyle = false;
-    });
-  }
-
-  prefs.watchKey('siteGap', updateStyle);
-  prefs.watchKey('siteSize', updateStyle);
-  prefs.watchKey('siteWidth', updateStyle);
-  updateStyle();
 }
 
-const bgImgEl = document.createElement('div');
-bgImgEl.className = 'main-bg-img';
-document.body.appendChild(bgImgEl);
-getAndWatch(local, StorageKey.bg, x => {
-  if (x) {
-    bgImgEl.style.backgroundImage = `url(${x})`;
-  }
-});
-
-initThemeHandler();
+handleStyleVar();
+handleDarkMode();
+handleBackgroundImage();
+handleOther();
